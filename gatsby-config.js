1f45b27248df2e18fdb43f5aa69170fe1b1fe68b
Lars Bekkema <lars.bekkema+gitlab@gmail.com>
2018-03-14 10:40:28 +0100

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,15 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-49905813-10",
+        head: false,
+        anonymize: true,
+        respectDNT: true,
+      },
+    },
+    {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
