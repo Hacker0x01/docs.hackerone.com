a9046e4b458d487edcbcc3f3bcae848d8a21b044
Lars Bekkema <lars.bekkema+gitlab@gmail.com>
2018-04-11 10:36:53 +0200

    Remove whitespace

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
             resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 500,
-              sizeByPixelDensity: true, 
+              sizeByPixelDensity: true,
             },
           },
         ],
