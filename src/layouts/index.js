e82b88ef6a32d4818e31eb3fe4003ddfe291ba6f
Reed Loden <reed@hackerone.com>
2018-05-14 15:25:47 -0700

    Update HTML tags

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -10,14 +10,36 @@ import '../css/main.scss'
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Product Documentation"
+      title={"Product Documentation"}
+      titleTemplate={`%s | HackerOne`}
       meta={[
         {
           name: 'description',
           content:
             'Get instant answers to the most common questions and learn how to use HackerOne.',
         },
+        {
+          name: 'og:type',
+          content: 'website',
+        },
+        {
+          name: 'og:site_name',
+          content: 'HackerOne Product Documentation',
+        },
+        {
+          name: 'twitter:card',
+          content: 'summary_large_image',
+        },
+        {
+          name: 'twitter:creator',
+          content: '@Hacker0x01',
+        },
+        {
+          name: 'twitter:site',
+          content: '@Hacker0x01',
+        },
       ]}
+      htmlAttributes={{lang: "en"}}
     />
     <Header />
     <div className="wrapper">{children()}</div>
