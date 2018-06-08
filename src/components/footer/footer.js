c4ad8eaceab3e04c25ccf64c7cef1c1a6b56cf2b
Reed Loden <reed@hackerone.com>
2018-06-08 13:51:31 -0700

    Use web contact forms rather than e-mail addresses

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -7,8 +7,8 @@ const Footer = () => (
   <div className="footer">
     <div className="footer__inner">
       We're always happy to help. Feel free to{' '}
-      <a href="mailto:support@hackerone.com">contact support</a>,{' '}
-      <a href="mailto:sales@hackerone.com">connect with our sales team</a>, or{' '}
+      <a href="https://support.hackerone.com/hc/en-us/requests/new">contact support</a>,{' '}
+      <a href="https://www.hackerone.com/contact">connect with our sales team</a>, or{' '}
       <a href="https://www.hackerone.com" target="_blank">
         check out our website
       </a>.
