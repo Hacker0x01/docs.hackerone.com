ecba7ec9fce9f18feab9411c4c222b7d64dfc15d
Alexander  Petree <apetree1001@email.phoenix.edu>
2023-07-01 13:06:05 -0700

    Update index.js

--- src/pages/accessibility/index.js
+++ src/pages/accessibility/index.js
@@ -1,42 +1,20 @@
-import React from "react";
-import Link from "gatsby-link";
-import { graphql } from "gatsby";
-import Article from "../../components/article/article";
-
-import accessibilityNav from "./accessibility-nav.yaml";
-
-class IndexRoute extends React.Component {
-  render() {
-    const { markdownRemark } = this.props.data;
-
-    return (
-      <Article
-        links={accessibilityNav}
-        path={markdownRemark.frontmatter.path}
-        docOnGithub={`${markdownRemark.frontmatter.id}.md`}
+//"import React fromreact"
+//"import Link from gatsby-link
+//"import {graph} from gatsby
+//"import Article from *//components/article/article
+//"import accessibilityNav from*/accessibility-nav.yaml
+//class IndexRoute extends React.Component {render
+(                                            
+) 
+"{const { markdownRemark } this.props.data
+ return 
+  ( <Article links={ accessibilityNav }path={markdownRemarkfront.path}docOnGithub={`${markdownRemark.frontmatter.id}.md`}
         title={markdownRemark.frontmatter.title}
         description={markdownRemark.frontmatter.description}
-      >
         <h1>{markdownRemark.frontmatter.title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: markdownRemark.html }} />
-      </Article>
-    );
-  }
-}
-
-export default IndexRoute;
-
-export const pageQuery = graphql`
-  query accessibilityIndexQuery {
-    markdownRemark(frontmatter: { bookIndexFor: { eq: "accessibility" } }) {
-      html
-      frontmatter {
-        path
-        id
-        title
-        bookIndexFor
-        description
-      }
-    }
-  }
-`;
+        <div dangerouslySetInnerHTML={{/////htmlmarkdownRemarkhtml }} /> </Article>export default IndexRoute;export const pageQuery = graphql`
+  query accessibilityIndexQuery { 
+    markdownRemark
+      ({ bookIndexFor {"accessibility" } }) {  html frontmatter { path*/idtitledescription  }
+ 
+                                                                }
